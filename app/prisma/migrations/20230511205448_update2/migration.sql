5f779d8332bff9d655c30f6c2c82dfcbd90173a3
NGhost07 <k59tranhoangnam@gmail.com>
2023-05-11 23:56:13 +0300

    change database

---
+++ app/prisma/migrations/20230511205448_update2/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - The values [ADMIN] on the enum `Role` will be removed. If these variants are still used in the database, this will fail.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "Role_new" AS ENUM ('LEARNER', 'TEACHER');
+ALTER TABLE "User" ALTER COLUMN "role" DROP DEFAULT;
+ALTER TABLE "User" ALTER COLUMN "role" TYPE "Role_new" USING ("role"::text::"Role_new");
+ALTER TYPE "Role" RENAME TO "Role_old";
+ALTER TYPE "Role_new" RENAME TO "Role";
+DROP TYPE "Role_old";
+ALTER TABLE "User" ALTER COLUMN "role" SET DEFAULT 'LEARNER';
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "password" TEXT NOT NULL;
